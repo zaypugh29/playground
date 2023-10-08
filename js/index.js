3ea9bebf56c81fa482877df4fb590f7470e79ae1
zay <zaypugh29@gmail.com>
2023-10-08 12:45:53 -0400

    dropdown with cards

--- js/index.js
+++ js/index.js
@@ -1,55 +1,41 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const axios_1 = __importDefault(require("axios"));
-function main() {
-    return __awaiter(this, void 0, void 0, function* () {
-        const request = () => __awaiter(this, void 0, void 0, function* () {
-            try {
-                const response = yield axios_1.default.get("https://swapi.dev/api/people");
-                return response.data.results;
-            }
-            catch (error) {
-                console.log("oh no :/");
-            }
-        });
-        const films = () => __awaiter(this, void 0, void 0, function* () {
-            try {
-                const films = yield axios_1.default.get("https://swapi.dev/api/films");
-                return films.data.results;
-            }
-            catch (error) {
-                console.log("Come again? ");
-            }
-        });
-        const peopleArr = yield request();
-        const filmArr = yield films();
-        // if (peopleArr){
-        //     for (let person of peopleArr) {
-        //         console.log(person.name)
-        //     }
-        // }
-        // if (filmArr) {
-        //     for (let film of filmArr){
-        //         console.log(film.title);
-        //     }
-        // }
-        const newFilmArray = filmArr === null || filmArr === void 0 ? void 0 : filmArr.map((x) => x.episode_id);
-        console.log(newFilmArray === null || newFilmArray === void 0 ? void 0 : newFilmArray.sort());
-    });
-}
-main();
+// async function main() {
+  
+
+
+
+//     const request = async () => {
+//         try {
+//             const response = await axios.get<response>("https://swapi.dev/api/people");
+//             return response.data.results;
+//         } catch (error) {
+//             console.log("oh no :/");
+//         }
+//     }
+//     const films = async () => {
+//         try {
+//             const films = await axios.get<movieResponse>("https://swapi.dev/api/films");
+//             return films.data.results;
+//         } catch (error) {
+//             console.log("Come again? ");
+//         }
+//     }
+//     const peopleArr = await request();
+//     const filmArr = await films();
+//     // if (peopleArr){
+//     //     for (let person of peopleArr) {
+//     //         console.log(person.name)
+//     //     }
+//     // }
+//     // if (filmArr) {
+//     //     for (let film of filmArr){
+//     //         console.log(film.title);
+//     //     }
+//     // }
+//     const newFilmArray = filmArr?.map((x) => x.episode_id);
+//     console.log(newFilmArray?.sort());
+// }
+// main();
 // The point of the 'await' keyword is so that you do not have to attach a .then to functions
 // that return promises.
 // These axios requests should be their own functions. Return the data, and then work with it
--- ts/index.ts
+++ ts/index.ts
@@ -51,51 +51,51 @@ interface movie {
     url?: string
 }
 
-async function main() {
+// async function main() {
 
-    const request = async () => {
-        try {
-            const response = await axios.get<response>("https://swapi.dev/api/people");
-            return response.data.results;
-        } catch (error) {
-            console.log("oh no :/");
-        }
-    }
+//     const request = async () => {
+//         try {
+//             const response = await axios.get<response>("https://swapi.dev/api/people");
+//             return response.data.results;
+//         } catch (error) {
+//             console.log("oh no :/");
+//         }
+//     }
 
-    const films = async () => {
-        try {
-            const films = await axios.get<movieResponse>("https://swapi.dev/api/films");
-            return films.data.results;
-        } catch (error) {
-            console.log("Come again? ");
-        }
-    }
+//     const films = async () => {
+//         try {
+//             const films = await axios.get<movieResponse>("https://swapi.dev/api/films");
+//             return films.data.results;
+//         } catch (error) {
+//             console.log("Come again? ");
+//         }
+//     }
 
-    const peopleArr = await request();
-    const filmArr = await films();
-
-
-    // if (peopleArr){
-    //     for (let person of peopleArr) {
-    //         console.log(person.name)
-    //     }
-    // }
-
-    // if (filmArr) {
-    //     for (let film of filmArr){
-    //         console.log(film.title);
-    //     }
-    // }
-
-    const newFilmArray = filmArr?.map( (x) => x.episode_id);
-    console.log(newFilmArray?.sort());
+//     const peopleArr = await request();
+//     const filmArr = await films();
 
 
+//     // if (peopleArr){
+//     //     for (let person of peopleArr) {
+//     //         console.log(person.name)
+//     //     }
+//     // }
+
+//     // if (filmArr) {
+//     //     for (let film of filmArr){
+//     //         console.log(film.title);
+//     //     }
+//     // }
+
+//     const newFilmArray = filmArr?.map((x) => x.episode_id);
+//     console.log(newFilmArray?.sort());
 
 
-}
 
-main();
+
+// }
+
+// main();
 
 // The point of the 'await' keyword is so that you do not have to attach a .then to functions
 // that return promises.
